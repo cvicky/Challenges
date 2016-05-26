8cc11787e1c6f773a7f1f226db13ae4af9eea34d
cvicky <c.vicky1213@gmail.com>
2016-05-26 02:08:25 -0400

    Update 344ReverseString.java

--- leetcode/344ReverseString.java
+++ leetcode/344ReverseString.java
@@ -16,3 +16,29 @@ public class Solution {
         return rev;
     }
 }
+
+
+//OR USE COLLECTIONS WHICH HAS A REVERSE METHOD
+
+/*
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public String reverseString(String s) {
+        String rev = "";
+        char[] sArray = s.toCharArray();
+        List<Character> lls = new LinkedList<>();
+        for(char c : sArray){
+            lls.add(c);
+        }
+        Collections.reverse(lls);
+        ListIterator li = lls.listIterator();
+        while(li.hasNext() ) {
+            rev = rev+li.next();
+        }
+        return rev;
+    }
+}
+
+*/
