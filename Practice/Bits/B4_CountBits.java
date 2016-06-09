1bd92e12ee780402b7f496f89100340855b30a5d
cvicky <c.vicky1213@gmail.com>
2016-06-09 16:10:10 -0400

    Update B4_CountBits.java

--- Practice/Bits/B4_CountBits.java
+++ Practice/Bits/B4_CountBits.java
@@ -43,5 +43,23 @@ public class B4_CountBits {
   
   public int countBitsEvenFast( int x){
     //count bits of each 2-bit chunk
+    x=x-((x >> 1) & (0x55555555);
+    //count bits of each 4-bit chunk
+    x=(x & 0x33333333) + ((x>>2) & 0x33333333);
+    //count bits of each 8-bit chunk
+    x= x+(x >> 4);
+    //mask out junk
+    x&= 0xF0F0F0F;
+    //add all four 8-bit chunks
+    return (x*0x01010101) >> 24;
+    
+  }
+  
+  public static void main(String args[]){
+    B4_CountBits cb = new B4_CountBits();
+    System.out.println(cb.countBits(3636363) );
+    System.out.println(cb.countBitsFaster(3636363) );
+    System.out.println(cb.countBitsEvenFaster(3636363) );
+    
   }
 }
