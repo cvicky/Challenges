f130dca50abf775c4f7375d5def8aef893bb06f0
cvicky <c.vicky1213@gmail.com>
2016-06-08 21:59:59 -0400

    Update B2_BitRotation.java

--- Practice/Bits/B2_BitRotation.java
+++ Practice/Bits/B2_BitRotation.java
@@ -29,3 +29,23 @@ In an arithmetic shift, the sign bit is extended to preserve the signedness of t
 For example: -2 represented in 8 bits would be 11111110 (because the most significant bit has negative weight). Shifting it right one bit using arithmetic shift would give you 11111111, or -1. Logical right shift, however, does not care that the value could possibly represent a number; it simply moves everything to the right and fills in from the left with 0s. Shifting our -2 right one bit using logical shift would give 01111111.
 */
 
+/*
+
+>>> is unsigned-shift; it'll insert 0. >> is signed, and will extend the sign bit.
+
+JLS 15.19 Shift Operators
+
+The shift operators include left shift <<, signed right shift >>, and unsigned right shift >>>.
+
+The value of n>>s is n right-shifted s bit positions with sign-extension.
+
+The value of n>>>s is n right-shifted s bit positions with zero-extension.
+    System.out.println(Integer.toBinaryString(-1));
+    // prints "11111111111111111111111111111111"
+    System.out.println(Integer.toBinaryString(-1 >> 16));
+    // prints "11111111111111111111111111111111"
+    System.out.println(Integer.toBinaryString(-1 >>> 16));
+    // prints "1111111111111111"
+
+
+*/
