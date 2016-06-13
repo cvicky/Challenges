d2b53dd13476c3b04aeffe02498c289729d7ddb1
cvicky <c.vicky1213@gmail.com>
2016-06-12 23:08:56 -0400

    Create SQ_7_MedianFinder.java

---
+++ Practice/StackQueue/SQ_7_MedianFinder.java
@@ -0,0 +1,21 @@
+//* 
+* Author: Vicky Chen
+* Date: 6/11/2016
+*
+*Find median in stream of numbers
+*/
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class SQ7_MedianFinder {
+  PriorityQueue<Integer> minPq = new PriorityQueue<>():
+  PriorityQueue<Integer> maxPq = new PriorityQueue<>();
+  
+  public SQ7_MedianFinder(){
+    minPq = new PriorityQueue<>()'
+    maxPq = new PriorityQueue<>(20, Collections.reverseOrder());
+  }
+  
+  //Adds a nmber into data structure
+}
