7ec7dda2567fc9739198c9fbd8768a3b258847fe
cvicky <c.vicky1213@gmail.com>
2016-10-05 03:17:37 -0400

    Create PythonDivision.py

---
+++ hackerrank/PythonDivision.py
@@ -0,0 +1,22 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+Task 
+Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+
+You don't need to perform any rounding or formatting operations.
+
+Input Format 
+The first line contains the first integer, . The second line contains the second integer, .
+
+Output Format 
+Print the two lines as described above.
+'''
+
+num1 = int(raw_input()) #convert string to int
+num2 = int(raw_input())
+
+intAns = num1//num2
+floatAns = num1/float(num2) #cast the denominator as a float, int/float = float
+
+print(intAns)
+print(floatAns)
