03d08ca40f56dafc77fa4155e0b5fb01e07ddf3d
cvicky <c.vicky1213@gmail.com>
2016-10-05 03:01:18 -0400

    Create WeirdNotWeird.py

---
+++ hackerrank/WeirdNotWeird.py
@@ -0,0 +1,21 @@
+#!/bin/python
+'''
+If  is odd, print Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20, print Not Weird
+
+'''
+import sys
+
+N = int(raw_input().strip())
+
+if N %2 != 0:
+    print("Weird")
+elif(N%2 ==0):
+    if (N>=2 and N<=5):
+        print("Not Weird")
+    elif(N>=6 and N<=20):
+        print("Weird")
+    elif(N>20):
+        print("Not Weird")
